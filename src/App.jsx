1d24f9ce1a0461f1baa8c39d5de02278b9104c68
AbzMijon <egorchiktop228@gmail.com>
2022-08-22 13:54:52 +0300

    make structure and settings  of project

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react"
+
+function App() {
+    return (
+        <React.Fragment>
+            <h3>Hello to do task!</h3>
+        </React.Fragment>
+    )
+}
+
+export default App;
