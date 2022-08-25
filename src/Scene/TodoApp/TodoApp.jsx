441779efbd4a596d958ecadc8df6fbdcfa8f4c22
AbzMijon <egorchiktop228@gmail.com>
2022-08-25 15:33:49 +0300

    add all functions of todo list

--- src/Scene/TodoApp/TodoApp.jsx
+++ src/Scene/TodoApp/TodoApp.jsx
@@ -7,27 +7,36 @@ import './todoApp.scss';
 function TodoApp() {
     const [todoValue, setTodoValue] = useState('');
     const [todos, setTodos] = useState([]);
-    console.log(todos);
+
+    const addTodo = () => {
+        setTodos([...todos, {text: todoValue, id: Math.ceil(Math.random() * 1000)}]);
+        setTodoValue('');
+    }
+
 
     return (
         <React.Fragment>
             <h1 className="main-title">Todo list</h1>
             <div className="todo">
-                {/* <Header todoValue={todoValue} setTodoValue={setTodoValue}/> */}
                 <header className="todo__header">
-                    <button type="button" className="todo__delete-all">Del all</button>
-                    <input className="todo__input" type="text" placeholder="Feed my pet..." value={todoValue} onChange={e => setTodoValue(e.target.value)} />
-                    <button type="button" className="todo__add" onClick={() => {
-                        setTodos([...todos, {text: todoValue}]);
-                        setTodoValue('');
-                    }}>Add</button>
+                    <button type="button" className="todo__delete-all" onClick={() => {
+                        setTodos([]);
+                    }}>Del all</button>
+                    <input onKeyDown={(keyPress) => {
+                        if(keyPress.keyCode === 13) addTodo();
+                    }} className="todo__input" type="text" placeholder="Feed my pet..." value={todoValue} onChange={e => setTodoValue(e.target.value)} />
+                    <button type="button" className="todo__add" onClick={addTodo}>Add</button>
                 </header>
                 <main className="todo__main">
                     <section className="todo__tasks">
-                        {todos.map(todo => {
+                        {todos.map((todo) => {
                             return (
-                                <div className="card">
-                                    <button className="card__close">x</button>
+                                <div className="card" key={todo.id}>
+                                    <button className="card__close" onClick={(e) => {
+                                        const cardID = todo.id;
+                                        const deletedTodoArray = todos.filter((todo) => todo.id !== cardID);
+                                        setTodos(deletedTodoArray);
+                                    }}>x</button>
                                     <p className="card__text">{todo.text}</p>
                                 </div>
                             )
